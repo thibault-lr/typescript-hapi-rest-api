5bf93612d5dd6ceac50b024599b763bf582a189f
thibault-saegus <thibault.leroy@saegus.com>
2019-06-19 15:58:48 +0200

    update environments variables

--- src/database.ts
+++ src/database.ts
@@ -10,9 +10,9 @@ class Database {
         type: "postgres",
         host: process.env.DB_HOST,
         port: Number(process.env.DB_PORT),
-        username: "postgres",
-        password: "postgres",
-        database: "analytics",
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
         entities: [User],
         synchronize: true,
         logging: true
